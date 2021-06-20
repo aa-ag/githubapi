e93ef13ee4ad4ab7a2430cd1d4df91b714850bb7
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-19 23:15:23 -0500

    returning json() made of request

--- githubapi.py
+++ githubapi.py
@@ -1,15 +1,17 @@
 ############------------ IMPORTS ------------############
 import requests
-
+import pprint
+import settings
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+github_token = settings.GITHUB_TOKEN
 
 ############------------ FUNCTION(S) ------------############
 def hit_end_point():
-    request = requests.get('https://github.com/aa-ag')
 
-    print(request.status_code)
-    # 200
+    request = requests.get('https://api.github.com/notifications', auth=('aa-ag', github_token))
+    
+    print(request.json())
 
 
 ############------------ DRIVER CODE ------------############
