555911a54c3d1d8842c4be8919f551eb330b7351
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-19 22:43:31 -0500

    added hit_end_point() function, getting 200 status code back due to successful connection

--- githubapi.py
+++ githubapi.py
@@ -1,7 +1,17 @@
 ############------------ IMPORTS ------------############
+import requests
+
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 
 ############------------ FUNCTION(S) ------------############
+def hit_end_point():
+    request = requests.get('https://github.com/aa-ag')
+
+    print(request.status_code)
+    # 200
+
 
 ############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    hit_end_point()
