8de395d3b2ebe48d8f62006ccfc558d763babe01
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-22 18:09:56 -0500

    added logic and comment on api's technical limit at 100 repos per page

--- githubapi.py
+++ githubapi.py
@@ -4,6 +4,16 @@ import pprint
 import settings
 import subprocess
 
+'''
+Requests that return multiple items will be paginated to 30 items by default. 
+You can specify further pages with the page parameter. 
+For some resources, you can also set a custom page size up to 100 with the per_page parameter. 
+Note that for technical reasons not all endpoints respect the per_page parameter, 
+see events for example.
+
+source: https://docs.github.com/en/rest/overview/resources-in-the-rest-api#pagination
+''' 
+
 ############------------ GLOBAL VARIABLE(S) ------------############
 github_auth = settings.GITHUB_AUTH
 
@@ -15,7 +25,7 @@ def hit_end_point():
      and prettyprints the jston with an 
      indentation of 4 spaces (2 tabs)
     '''
-    url = 'https://api.github.com/users/aa-ag/repos?per_page=144'
+    url = 'https://api.github.com/users/aa-ag/repos?per_page=200'
 
     request = requests.get(url, data=github_auth)
     
@@ -24,8 +34,8 @@ def hit_end_point():
     # pp = pprint.PrettyPrinter(indent=4)
     # pp.pprint(request)
 
-    for repo in request:
-        print(repo['html_url'])
+    for number, repo in enumerate(request):
+        print(number + 1, repo['html_url'])
 
 
 ############------------ DRIVER CODE ------------############
@@ -33,104 +43,104 @@ if __name__ == "__main__":
     hit_end_point()
 
 '''
-https://github.com/aa-ag/5artists_Angular
-https://github.com/aa-ag/About_Time
-https://github.com/aa-ag/astp
-https://github.com/aa-ag/audiobooks
-https://github.com/aa-ag/Avengers_Phonebook
-https://github.com/aa-ag/bicycles_rental
-https://github.com/aa-ag/bitcoinprice
-https://github.com/aa-ag/blog
-https://github.com/aa-ag/bootstrap
-https://github.com/aa-ag/calculator
-https://github.com/aa-ag/camelstuff
-https://github.com/aa-ag/chipy.org
-https://github.com/aa-ag/clock.js
-https://github.com/aa-ag/commands
-https://github.com/aa-ag/computer-science
-https://github.com/aa-ag/cookiescacheclear
-https://github.com/aa-ag/counting_stuff
-https://github.com/aa-ag/cpython
-https://github.com/aa-ag/css-animations
-https://github.com/aa-ag/CSVstuff
-https://github.com/aa-ag/curriculum
-https://github.com/aa-ag/dadjokes
-https://github.com/aa-ag/deleteemails
-https://github.com/aa-ag/delhistory
-https://github.com/aa-ag/designing-programs
-https://github.com/aa-ag/developer-roadmap
-https://github.com/aa-ag/dirtree
-https://github.com/aa-ag/discrete
-https://github.com/aa-ag/dmwp
-https://github.com/aa-ag/domain_data
-https://github.com/aa-ag/download
-https://github.com/aa-ag/ds_and_a_projects
-https://github.com/aa-ag/emc2
-https://github.com/aa-ag/emptytrash
-https://github.com/aa-ag/endecrypter
-https://github.com/aa-ag/f1_stangins
-https://github.com/aa-ag/filecount
-https://github.com/aa-ag/fileuploader
-https://github.com/aa-ag/first-contributions
-https://github.com/aa-ag/flaskapi
-https://github.com/aa-ag/freecodecamp1outof5
-https://github.com/aa-ag/freecodecamp2outof5
-https://github.com/aa-ag/freecodecamp3outof5
-https://github.com/aa-ag/freecodecamp4outof5
-https://github.com/aa-ag/freecodecamp5outof5
-https://github.com/aa-ag/fullstack
-https://github.com/aa-ag/fullstack-nanodegree-vm
-https://github.com/aa-ag/gamapi
-https://github.com/aa-ag/githubapi
-https://github.com/aa-ag/github_collab_lab
-https://github.com/aa-ag/gitignore
-https://github.com/aa-ag/googlesearch
-https://github.com/aa-ag/groomer
-https://github.com/aa-ag/hangman
-https://github.com/aa-ag/haystack
-https://github.com/aa-ag/hofvidz
-https://github.com/aa-ag/HTMLHint
-https://github.com/aa-ag/ignoresnippet
-https://github.com/aa-ag/I_Know_Sports
-https://github.com/aa-ag/jsprojects
-https://github.com/aa-ag/JS_funfacts
-https://github.com/aa-ag/launchurls
-https://github.com/aa-ag/LawFirmSite
-https://github.com/aa-ag/lol
-https://github.com/aa-ag/lunchreminder
-https://github.com/aa-ag/magic8ball
-https://github.com/aa-ag/markets
-https://github.com/aa-ag/miniremesh
-https://github.com/aa-ag/movefiles
-https://github.com/aa-ag/movie_theater_database
-https://github.com/aa-ag/nano
-https://github.com/aa-ag/off
-https://github.com/aa-ag/orbit-model
-https://github.com/aa-ag/passwordcracker
-https://github.com/aa-ag/passwordgenerator
-https://github.com/aa-ag/pcc_2e
-https://github.com/aa-ag/pdfs
-https://github.com/aa-ag/pg13
-https://github.com/aa-ag/phcopy
-https://github.com/aa-ag/phonenums
-https://github.com/aa-ag/piedpiper
-https://github.com/aa-ag/piglatintranslator
-https://github.com/aa-ag/project_garage
-https://github.com/aa-ag/project_roi
-https://github.com/aa-ag/project_sam
-https://github.com/aa-ag/pylexa
-https://github.com/aa-ag/python-guide
-https://github.com/aa-ag/ratings
-https://github.com/aa-ag/recipes
-https://github.com/aa-ag/redditclone
-https://github.com/aa-ag/reformat
-https://github.com/aa-ag/RESTcountries
-https://github.com/aa-ag/scraper
-https://github.com/aa-ag/sec
-https://github.com/aa-ag/sendemails
-https://github.com/aa-ag/sentiment
-https://github.com/aa-ag/signin_final
-https://github.com/aa-ag/simple-rss-reader
-https://github.com/aa-ag/slacknot
-https://github.com/aa-ag/sms
+1 https://github.com/aa-ag/5artists_Angular
+2 https://github.com/aa-ag/About_Time
+3 https://github.com/aa-ag/astp
+4 https://github.com/aa-ag/audiobooks
+5 https://github.com/aa-ag/Avengers_Phonebook
+6 https://github.com/aa-ag/bicycles_rental
+7 https://github.com/aa-ag/bitcoinprice
+8 https://github.com/aa-ag/blog
+9 https://github.com/aa-ag/bootstrap
+10 https://github.com/aa-ag/calculator
+11 https://github.com/aa-ag/camelstuff
+12 https://github.com/aa-ag/chipy.org
+13 https://github.com/aa-ag/clock.js
+14 https://github.com/aa-ag/commands
+15 https://github.com/aa-ag/computer-science
+16 https://github.com/aa-ag/cookiescacheclear
+17 https://github.com/aa-ag/counting_stuff
+18 https://github.com/aa-ag/cpython
+19 https://github.com/aa-ag/css-animations
+20 https://github.com/aa-ag/CSVstuff
+21 https://github.com/aa-ag/curriculum
+22 https://github.com/aa-ag/dadjokes
+23 https://github.com/aa-ag/deleteemails
+24 https://github.com/aa-ag/delhistory
+25 https://github.com/aa-ag/designing-programs
+26 https://github.com/aa-ag/developer-roadmap
+27 https://github.com/aa-ag/dirtree
+28 https://github.com/aa-ag/discrete
+29 https://github.com/aa-ag/dmwp
+30 https://github.com/aa-ag/domain_data
+31 https://github.com/aa-ag/download
+32 https://github.com/aa-ag/ds_and_a_projects
+33 https://github.com/aa-ag/emc2
+34 https://github.com/aa-ag/emptytrash
+35 https://github.com/aa-ag/endecrypter
+36 https://github.com/aa-ag/f1_stangins
+37 https://github.com/aa-ag/filecount
+38 https://github.com/aa-ag/fileuploader
+39 https://github.com/aa-ag/first-contributions
+40 https://github.com/aa-ag/flaskapi
+41 https://github.com/aa-ag/freecodecamp1outof5
+42 https://github.com/aa-ag/freecodecamp2outof5
+43 https://github.com/aa-ag/freecodecamp3outof5
+44 https://github.com/aa-ag/freecodecamp4outof5
+45 https://github.com/aa-ag/freecodecamp5outof5
+46 https://github.com/aa-ag/fullstack
+47 https://github.com/aa-ag/fullstack-nanodegree-vm
+48 https://github.com/aa-ag/gamapi
+49 https://github.com/aa-ag/githubapi
+50 https://github.com/aa-ag/github_collab_lab
+51 https://github.com/aa-ag/gitignore
+52 https://github.com/aa-ag/googlesearch
+53 https://github.com/aa-ag/groomer
+54 https://github.com/aa-ag/hangman
+55 https://github.com/aa-ag/haystack
+56 https://github.com/aa-ag/hofvidz
+57 https://github.com/aa-ag/HTMLHint
+58 https://github.com/aa-ag/ignoresnippet
+59 https://github.com/aa-ag/I_Know_Sports
+60 https://github.com/aa-ag/jsprojects
+61 https://github.com/aa-ag/JS_funfacts
+62 https://github.com/aa-ag/launchurls
+63 https://github.com/aa-ag/LawFirmSite
+64 https://github.com/aa-ag/lol
+65 https://github.com/aa-ag/lunchreminder
+66 https://github.com/aa-ag/magic8ball
+67 https://github.com/aa-ag/markets
+68 https://github.com/aa-ag/miniremesh
+69 https://github.com/aa-ag/movefiles
+70 https://github.com/aa-ag/movie_theater_database
+71 https://github.com/aa-ag/nano
+72 https://github.com/aa-ag/off
+73 https://github.com/aa-ag/orbit-model
+74 https://github.com/aa-ag/passwordcracker
+75 https://github.com/aa-ag/passwordgenerator
+76 https://github.com/aa-ag/pcc_2e
+77 https://github.com/aa-ag/pdfs
+78 https://github.com/aa-ag/pg13
+79 https://github.com/aa-ag/phcopy
+80 https://github.com/aa-ag/phonenums
+81 https://github.com/aa-ag/piedpiper
+82 https://github.com/aa-ag/piglatintranslator
+83 https://github.com/aa-ag/project_garage
+84 https://github.com/aa-ag/project_roi
+85 https://github.com/aa-ag/project_sam
+86 https://github.com/aa-ag/pylexa
+87 https://github.com/aa-ag/python-guide
+88 https://github.com/aa-ag/ratings
+89 https://github.com/aa-ag/recipes
+90 https://github.com/aa-ag/redditclone
+91 https://github.com/aa-ag/reformat
+92 https://github.com/aa-ag/RESTcountries
+93 https://github.com/aa-ag/scraper
+94 https://github.com/aa-ag/sec
+95 https://github.com/aa-ag/sendemails
+96 https://github.com/aa-ag/sentiment
+97 https://github.com/aa-ag/signin_final
+98 https://github.com/aa-ag/simple-rss-reader
+99 https://github.com/aa-ag/slacknot
+100 https://github.com/aa-ag/sms
 '''
