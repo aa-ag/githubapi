180235dc15bcfa311dbb0f0dea5cb527f7fff1b0
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-22 17:55:05 -0500

    added docstring to document/comment hit_end_point()

--- githubapi.py
+++ githubapi.py
@@ -8,6 +8,12 @@ github_auth = settings.GITHUB_AUTH
 
 ############------------ FUNCTION(S) ------------############
 def hit_end_point():
+    '''
+     imports github auth from settings,
+     hits enpoint, creates a json with it, 
+     and prettyprints the jston with an 
+     indentation of 4 spaces (2 tabs)
+    '''
     request = requests.get('https://api.github.com/users/aa-ag/repos', data=github_auth)
     request = request.json()
     pp = pprint.PrettyPrinter(indent=4)
