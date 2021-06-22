cbd1adc5bc3339d85a1efeec5b8b30e6cbfd2abd
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-22 17:53:20 -0500

    refactored githubapi.py to pretty print() json gotten back from requests.get() the endpoint

--- githubapi.py
+++ githubapi.py
@@ -8,10 +8,10 @@ github_auth = settings.GITHUB_AUTH
 
 ############------------ FUNCTION(S) ------------############
 def hit_end_point():
-
     request = requests.get('https://api.github.com/users/aa-ag/repos', data=github_auth)
-    
-    print(request.status_code)
+    request = request.json()
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(request)
 
 
 ############------------ DRIVER CODE ------------############
