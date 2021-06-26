c845f5131ede10d300d8b9bb376c32c4e16ad526
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-26 14:57:38 -0500

    added get_one_object_to_check_keys_and_values() function to experiment looking for pattern that would allow me to ignore forked repos

--- githubapi.py
+++ githubapi.py
@@ -2,6 +2,7 @@
 import requests
 import settings
 import subprocess
+import pprint
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 github_auth = settings.GITHUB_AUTH
@@ -46,6 +47,20 @@ def hit_end_point():
         page += 1
 
 
+def get_one_object_to_check_keys_and_values():
+    global github_auth
+
+    url = 'https://api.github.com/repos/aa-ag/cpython'
+
+    request = requests.get(url, data=github_auth)
+        
+    print(request.status_code)
+
+    
+
+
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    hit_end_point()
+    # hit_end_point()
+
+    get_one_object_to_check_keys_and_values()
