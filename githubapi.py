3255a87e0135bfc7f7bfb69919999ac7ab548b20
Aaron Aguerrevere <aaronaguerrevere@Aarons-MacBook-Pro.local>
2021-06-26 01:20:13 -0500

    fixed a few typos in docstrings

--- githubapi.py
+++ githubapi.py
@@ -1,70 +1,38 @@
 ############------------ IMPORTS ------------############
 import requests
-import pprint
 import settings
 import subprocess
-import csv
-
-'''
-Requests that return multiple items will be paginated to 30 items by default. 
-You can specify further pages with the page parameter. 
-For some resources, you can also set a custom page size up to 100 with the per_page parameter. 
-Note that for technical reasons not all endpoints respect the per_page parameter, 
-see events for example.
-
-source: https://docs.github.com/en/rest/overview/resources-in-the-rest-api#pagination
-''' 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 github_auth = settings.GITHUB_AUTH
 destination_path = settings.DESTINATION
 
 
-
 ############------------ FUNCTION(S) ------------############
-def save_locally():
+def save_locally(repo_being_cloned):
     '''
      helper function to `clone` each repo
-     locally to a give path -- for now
-     manually adding url & path
+     locally to a give path
     '''
-    repo_being_cloned = 'https://github.com/aa-ag/groomer'
+    global destination_path
+
     path_where_repo_will_be_cloned_to = destination_path
 
     command = ["git", "clone", repo_being_cloned]
 
     subprocess.run(command, cwd=path_where_repo_will_be_cloned_to)
 
-    '''
-     If cwd is not None, the function changes the working directory
-     to cwd before executing the child.
-    '''
-
 
 def hit_end_point():
     '''
      imports github auth from settings,
-     hits enpoint, creates a json with it, 
-     and prettyprints the jston with an 
-     indentation of 4 spaces (2 tabs)
+     hits endpoint, creates a json with it, 
+     then saving each locally to given path
     '''
     global github_auth
 
     page = 1
-    n = 1
 
-    dictionary_for_local_copy = dict()
-    
-    # #### check requests status to see if limit was hit
-    # url = f'https://api.github.com/users/aa-ag/repos?page={page}&per_page=100'
-
-    # request = requests.get(url, data=github_auth)
-
-    # print(request.status_code)
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(request)
-
-    ### actual code
     while page < 3:
         url = f'https://api.github.com/users/aa-ag/repos?page={page}&per_page=100'
 
@@ -73,22 +41,11 @@ def hit_end_point():
         request = request.json()
 
         for repo in request:
-            dictionary_for_local_copy[n] = repo['html_url']
-            n += 1
+            save_locally(repo['html_url'])
 
         page += 1
-
-    local_copy_just_in_case = open('local_copy.csv', 'w')
-
-    writer = csv.writer(local_copy_just_in_case)
-
-    for k, v in dictionary_for_local_copy.items():
-        writer.writerow([k, v])
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    # hit_end_point()
     hit_end_point()
-    # 403
-    # save_locally()
